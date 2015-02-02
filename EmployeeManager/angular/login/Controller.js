9c9a39011c2b3cc90d4f24ddfef6bb7fab578133
rakib-cse-sust <rakib.cse.sust@gmail.com>
2015-02-02 19:32:07 +0600

    a

---
+++ EmployeeManager/angular/login/Controller.js
@@ -0,0 +1,13 @@
+﻿employeeLoginApp.controller('EmployeeLoginController', function ($scope, $routeParams, $filter, employeeService, $location, $modal) {
+
+    init();
+
+    function init() {
+    }
+
+    $scope.login = function () {
+        alert();
+
+    };
+
+});
